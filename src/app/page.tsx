930844468c4b9b063315313d6567151f6fbf192e
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2025-10-18 13:43:25 +0530

    feat: init trpc & tanstack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-    return <div>Hello World</div>;
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+import { Suspense } from "react";
+
+export default async function Home() {
+    const queryClient = getQueryClient();
+
+    void queryClient.prefetchQuery(trpc.getUser.queryOptions());
+
+    return (
+        <div className="min-h-screen min-w-screen flex items-center justify-center">
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<p className="text-2xl">Loading...</p>}>
+                    <Client />
+                </Suspense>
+            </HydrationBoundary>
+        </div>
+    );
 }
