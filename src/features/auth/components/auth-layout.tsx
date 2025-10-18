f9e9ec2c9d41525c0999a7ae5588188f5113134b
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2025-10-18 20:50:37 +0530

    feat: add tweakcn theme & logos

---
+++ src/features/auth/components/auth-layout.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="bg-muted min-h-svh flex flex-col justify-center items-center gap-6 p-6 md:p-10">
+            <div className="flex w-full max-w-lg flex-col gap-8">
+                <Link
+                    href="/"
+                    className="flex items-center gap-4 self-center font-medium text-3xl"
+                >
+                    <Image
+                        alt="nodebase"
+                        src="/logos/logo.svg"
+                        width={40}
+                        height={40}
+                    />
+                    Nodebase
+                </Link>
+                {children}
+            </div>
+        </div>
+    );
+}
