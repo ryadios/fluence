46db1612b37511c3a9bbee4c04c099fdfe398d8f
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2025-10-20 05:40:49 +0530

    feat: add ui state & remove workflow option

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -5,15 +5,24 @@ import {
     EntityContainer,
     EntitySearch,
     EntityPagination,
+    LoadingView,
+    ErrorView,
+    EmptyView,
+    EntityList,
+    EntityItem,
 } from "@/components/entity-components";
 import {
     useCreateWorkflow,
+    useRemoveWorkflow,
     useSuspenseWorkflows,
 } from "../hooks/use-workflows";
 import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 import { useRouter } from "next/navigation";
 import { useWorkflowsParams } from "../hooks/use-workflows-params";
 import { useEntitySearch } from "@/hooks/use-entity-search";
+import type { Workflow } from "@/generated/prisma";
+import { WorkflowIcon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
 
 export function WorkflowsSearch() {
     const [params, setParams] = useWorkflowsParams();
@@ -34,9 +43,12 @@ export function WorkflowsList() {
     const workflows = useSuspenseWorkflows();
 
     return (
-        <div className="flex flex-1 justify-center items-center">
-            <p>{JSON.stringify(workflows.data, null, 2)}</p>
-        </div>
+        <EntityList
+            items={workflows.data.items}
+            getKey={(workflow) => workflow.id}
+            renderItem={(workflow) => <WorkflowItem data={workflow} />}
+            emptyView={<WorkflowsEmpty />}
+        />
     );
 }
 
@@ -92,10 +104,78 @@ export function WorkflowsContainer({
     return (
         <EntityContainer
             header={<WorkflowsHeader />}
-            search={<WorkflowsSearch></WorkflowsSearch>}
-            pagination={<WorkflowsPagination></WorkflowsPagination>}
+            search={<WorkflowsSearch />}
+            pagination={<WorkflowsPagination />}
         >
             {children}
         </EntityContainer>
+    );
+}
+
+export function WorkflowsLoading() {
+    return <LoadingView message="Loading workflows..." />;
+}
+
+export function WorkflowsError() {
+    return <ErrorView message="Error loading workflows..." />;
+}
+
+export function WorkflowsEmpty() {
+    const router = useRouter();
+    const createWorkflow = useCreateWorkflow();
+    const { handleError, modal } = useUpgradeModal();
+
+    const handleCreate = () => {
+        createWorkflow.mutate(undefined, {
+            onSuccess: (data) => router.push(`/workflows/${data.id}`),
+            onError: (err) => handleError(err),
+        });
+    };
+    return (
+        <>
+            {modal}
+            <EmptyView
+                onNew={handleCreate}
+                title="No workflows found"
+                message="You haven't created any workflows yet. Get started by creating your first workflow"
+                buttonLabel="Create Workflow"
+            />
+        </>
+    );
+}
+
+export function WorkflowItem({ data }: { data: Workflow }) {
+    const removeWorkflow = useRemoveWorkflow();
+
+    const handleRemove = () => {
+        removeWorkflow.mutate({
+            id: data.id,
+        });
+    };
+
+    return (
+        <EntityItem
+            href={`/workflows/${data.id}`}
+            title={data.name}
+            subtitle={
+                <>
+                    Updated{" "}
+                    {formatDistanceToNow(data.updatedAt, {
+                        addSuffix: true,
+                    })}{" "}
+                    &bull; Created{" "}
+                    {formatDistanceToNow(data.createdAt, {
+                        addSuffix: true,
+                    })}{" "}
+                </>
+            }
+            image={
+                <div className="size-8 flex items-center justify-center">
+                    <WorkflowIcon className="size-5 text-muted-foreground" />
+                </div>
+            }
+            onRemove={handleRemove}
+            isRemoving={removeWorkflow.isPending}
+        />
     );
 }
--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -1,5 +1,8 @@
-import { defaultShouldDehydrateQuery, QueryClient } from "@tanstack/react-query";
-// import superjson from "superjson";
+import {
+    defaultShouldDehydrateQuery,
+    QueryClient,
+} from "@tanstack/react-query";
+import superjson from "superjson";
 export function makeQueryClient() {
     return new QueryClient({
         defaultOptions: {
@@ -7,12 +10,13 @@ export function makeQueryClient() {
                 staleTime: 30 * 1000,
             },
             dehydrate: {
-                // serializeData: superjson.serialize,
+                serializeData: superjson.serialize,
                 shouldDehydrateQuery: (query) =>
-                    defaultShouldDehydrateQuery(query) || query.state.status === "pending",
+                    defaultShouldDehydrateQuery(query) ||
+                    query.state.status === "pending",
             },
             hydrate: {
-                // deserializeData: superjson.deserialize,
+                deserializeData: superjson.deserialize,
             },
         },
     });
